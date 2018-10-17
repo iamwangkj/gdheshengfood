2c6c906bde595cea7582c8a75bcd25084018bf73
wangkj <wangkaijie@odc.cmbchina.cn>
2018-10-17 17:32:07 +0800

    theme font color #606266

--- src/App.vue
+++ src/App.vue
@@ -57,4 +57,9 @@ export default {
   margin: 0;
   padding: 0;
 }
+
+#app {
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  color: #606266;
+}
 </style>
